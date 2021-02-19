12edbaf1e3e3d9c6e70df25dc6d4a1e574916480
Kevin Voell <KevinVoell@users.noreply.github.com>
2021-02-18 18:53:14 -0700

    Initial commit

---
+++ cs/HostedApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HostedApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Test(string s)
+        {
+            Console.WriteLine("Hello from the Hosted App");
+            Console.WriteLine(s);
+            return 0;
+        }
+    }
+}
